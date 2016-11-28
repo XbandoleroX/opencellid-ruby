ec2d49474a9500cc27743e504d24e44ca9d3439c
julianstafari <julianstafari@users.noreply.github.com>
2016-11-28 02:37:22 -0600

    First commit
    first commit

---
+++ csv_class.rb
@@ -0,0 +1,107 @@
+=begin
+____/\\\\\\\\\\\\\\\\\__/\\\\\\\\\\\\\\\\____/\\\________/\\\\\\\\\\\____IT4S______
+____\////////\\\//////__\///////\\\//////___/\\\\\______/\\\/////////\\\_TASK______
+__________\/\\\\___________\/\\\__________/\\\/\\\_____\//\\\______\///_FORCE______
+__________\/\\\\___________\/\\\________/\\\/\/\\\______\////\\\___________________
+__________\/\\\\___________\/\\\______/\\\/__\/\\\_________\////\\\________________
+__________\/\\\\___________\/\\\____/\\\\\\\\\\\\\\\\_________\////\\\_____________
+_________\\/\\\\___________\/\\\___\///////////\\\//___/\\\______\//\\\____________
+_____/\\\\\\\\\\\\\\\\\____\/\\\_____________\/\\\____\///\\\\\\\\\\\/_@IT4sMx_____
+____\/////////////////_____\///______________\///_______\///////////_@julianstafari
+=end
+
+require 'csv'
+require 'open-uri'
+require './database_class.rb'
+
+
+class Csv_class
+
+
+  def initialize
+
+    @csv_file = 'cell_towers.csv'
+    @csv_obj = nil
+    @db_obj = Cassandra_db.new
+    @db_obj.open_connection
+    @db_obj.connect_keyspace
+    @api_key = '' #YOUR API KEY OF OPENCELLID HERE!
+
+  end
+
+
+  def open_csv
+
+    @csv_obj = CSV.open(@csv_file)
+
+  end
+
+  def close_csv
+
+    @csv_obj.close
+
+  end
+
+  def operate
+
+    anal = 0
+    @csv_obj.each do |row|
+
+
+      puts anal
+      @db_obj.insert_row(row)
+      anal += 1
+      #TRY THIS ONE IF OU HAVING PROBLEMS WITH THE CASSANDRA INSERTS (SCRIPT BROKE!)
+      #WHERE xxxxx IS THE LAST ROW INSERTED
+      #if anal >= xxxxx
+      #  puts anal
+      #  @db_obj.insert_row(row)
+      #end
+      #anal += 1
+    end
+
+
+  end
+
+  def delete_past_csv
+
+    begin
+
+      #File.delete('cell_tower.csv.gz') if File.file? 'cell_tower.csv.gz'
+      File.delete('cell_tower.csv') if File.file? 'cell_tower.csv'
+      return true
+
+    rescue
+
+      return false
+
+    end
+
+  end
+
+  def decompress_gz(file)
+    #The simpliest way!
+    exec("gzip -d cell_tower.csv.gz ")
+
+  end
+
+  def get_csv
+
+    begin
+
+      delete_past_csv
+      link = "http://opencellid.org/downloads/?apiKey=".concat(@api_key).concat("&filename=cell_tower.csv.gz")
+      tmp_down = open(link)
+      IO.copy_stream(tmp_down, Dir.pwd.to_s.concat('/cell_tower.csv.gz'))
+      return true
+
+    rescue
+
+      puts "Some error downloading cell_towers.csv file! #{get_csv.class}"
+
+    end
+
+
+  end
+
+end
---
+++ database_class.rb
@@ -0,0 +1,149 @@
+require 'cassandra'
+
+=begin
+____/\\\\\\\\\\\\\\\\\__/\\\\\\\\\\\\\\\\____/\\\________/\\\\\\\\\\\____IT4S______
+____\////////\\\//////__\///////\\\//////___/\\\\\______/\\\/////////\\\_TASK______
+__________\/\\\\___________\/\\\__________/\\\/\\\_____\//\\\______\///_FORCE______
+__________\/\\\\___________\/\\\________/\\\/\/\\\______\////\\\___________________
+__________\/\\\\___________\/\\\______/\\\/__\/\\\_________\////\\\________________
+__________\/\\\\___________\/\\\____/\\\\\\\\\\\\\\\\_________\////\\\_____________
+_________\\/\\\\___________\/\\\___\///////////\\\//___/\\\______\//\\\____________
+_____/\\\\\\\\\\\\\\\\\____\/\\\_____________\/\\\____\///\\\\\\\\\\\/_@IT4sMx_____
+____\/////////////////_____\///______________\///_______\///////////_@julianstafari
+=end
+
+class Cassandra_db
+
+  def initialize
+
+    @obj_cluster = nil
+    @user = '' #YOUR CASSANDRA USER HERE
+    @password = '' #YOUR CASSANDRA PASSWORD HERE
+    #@database='tracking'
+    @hosts = ['localhost']
+    @keyspace = 'opencellid'
+    @session = nil
+
+  end
+  attr_accessor :keyspace
+
+
+################Connections################
+  def open_connection
+    begin
+
+      @obj_cluster = Cassandra.cluster(username: @user,
+                                       password: @password,
+                                       hosts: @hosts)
+
+    rescue => e
+      puts 'Error connection to cassandra cluster'
+      puts e
+      return false
+    end
+
+    return true
+
+  end
+
+  def connect_keyspace
+
+    begin
+      @session = @obj_cluster.connect(@keyspace)
+      return true
+    rescue
+      return false
+    end
+
+  end
+
+  def close_connection
+    begin
+      @session.close
+      return true
+    rescue => e
+      return false
+
+    end
+
+  end
+############################################
+
+###########CREATION_OF_KEYSPACE#############
+  def create_keyspace
+    begin
+
+      keyspace_definition = <<-KSDEF
+        CREATE KEYSPACE opencellid
+        WITH REPLICATION = {
+          'class': 'SimpleStrategy',
+          'replication_factor': 3
+        }
+      KSDEF
+
+      @session.execute(keyspace_definition)
+      #@session.execute("CREATE KEYSPACE opencellid WITH replication={'class':'SimpleStrategy','replication_factor':3};")
+      return true
+    rescue
+      puts "Error creating keyspace \"opencellid\" class: #{create_keyspace.class}"
+      return false
+    end
+
+  end
+
+  def check_ks_existence
+    begin
+      @obj_cluster.inspect.include?("opencellid") ? true :  false
+    rescue
+      puts "Error retriving available keyspaces #{check_ks_existence.class}"
+    end
+
+  end
+
+  def create_table
+
+    begin
+      @session.execute("create table opencellid.antennas(antenna text primary key, radio text, mcc text, mnc text, lac text, cid text, unit text, lon text, lat text, range text, averagesignal text);")
+      true
+    rescue
+      puts "Error trying to create table! #{create_table.class}"
+      false
+    end
+
+  end
+
+  def check_table_existence
+
+    begin
+      tmp_res = @session.execute("select table_name from system_schema.tables where keyspace_name='opencellid'").each do |row|
+
+        if row.has_value?"antennas"
+          return true
+        end
+
+      end
+      return false
+    rescue
+      puts "Error retriving available tables! #{check_table_existence.class}"
+      false
+    end
+
+  end
+
+  def insert_row(row)
+
+    begin
+      tmp_antenna = row[1].concat("-").concat(row[2]).concat("-").concat(row[3]).concat("-").concat(row[4])
+      statement = "insert into antennas(antenna, radio, mcc, mnc, lac, cid, unit, lon, lat, range, averagesignal) values('#{tmp_antenna}','#{row[0]}','#{row[1]}','#{row[2]}','#{row[3]}','#{row[4]}','#{row[5]}','#{row[6]}','#{row[7]}','#{row[8]}','#{row[13]}')"
+      puts statement
+      @session.execute(statement)
+      true
+    rescue
+      puts "Error trying to insert row! #{create_table.class}"
+      false
+    end
+
+  end
+
+
+end
